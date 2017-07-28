38592ef785de9db4f92ba502ff570279a4b83448
Helio Alves <helios25@gmail.com>
2017-07-28 11:53:46 -0400

    added fetchSongMutation and linked it with SongDetails component

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -2,29 +2,53 @@ import React, { Component } from 'react';
 import {
 	graphql
 } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
 import fetchSongQuery from '../queries/fetchSong';
 
 class SongDetails extends Component {
+	renderLyrics() {
+		const { song } = this.props.data;
+		if (song.lyrics) {
+			return (<ul>
+				{song.lyrics.map(({ id, likes, content}) => <li key={id}>Lyric Id {id} --- {content} - <span>{likes} likes</span></li>)}
+			</ul>)
+		}
+	}
+
 	render() {
 		const { loading, error, song } = this.props.data;
 
+		if (!song) {
+			return (
+				<div>
+					<p>There's no song that match this id: {this.props.match.params.id}</p>
+					<Link to='/'>Back</Link>
+				</div>
+			)
+		}
+
 		if (loading) {
-			return <p>Loading...</p>
+			return <p>Loading...</p>;
 		}
+
 		if (error) {
-			return <p>{error.message}</p>
+			return <p>{error.message}</p>;
 		}
+
 		return (
 			<div>
+				<Link to='/'>Back</Link>
 				<h3>{song.title}</h3>
-				<ul>
-					{song.lyrics.map(lyric => <li key={lyric.id}>{lyric.content}<span>{lyric.likes} likes</span></li>)}
-				</ul>
+				{this.renderLyrics()}
 			</div>
 		);
 	}
 }
 
-export default graphql(fetchSongQuery)(SongDetails);
+export default graphql(fetchSongQuery, {
+	options: (props) => {
+		return { variables: { id: props.match.params.id } };
+	}
+})(SongDetails);
 
