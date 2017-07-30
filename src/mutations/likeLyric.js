a2895e3aa457ef2c2a686ee0fee5cd6dda57ea5d
Helio Alves <helios25@gmail.com>
2017-07-29 22:46:25 -0400

    fixes to likeLyric

---
+++ src/mutations/likeLyric.js
@@ -0,0 +1,11 @@
+import { gql } from 'react-apollo';
+
+const likeLyricMutation = gql`
+	mutation LikeLyric($id: String) {
+		likeLyric(id:$id) {
+			likes
+		}
+	}
+`;
+
+export default likeLyricMutation;
